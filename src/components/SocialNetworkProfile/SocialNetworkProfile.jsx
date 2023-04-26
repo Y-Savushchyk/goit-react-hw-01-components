eb01ebd98f490f835b89d7fe74e9b1226615ac71
Y-Savushchyk <savushchky@gmail.com>
2023-04-26 23:19:52 +0300

    add icon

--- src/components/SocialNetworkProfile/SocialNetworkProfile.jsx
+++ src/components/SocialNetworkProfile/SocialNetworkProfile.jsx
@@ -1,7 +1,7 @@
 import css from './SocialNetworkProfile.module.css';
 import PropTypes from 'prop-types';
 import user from '../../user.json'
-
+import { MdAlternateEmail } from 'react-icons/md';
 export const SocialNetworkProfile =({username, tag, location, avatar, stats}) =>{
     return (<div className={css.profile}>
     <div className={css.description}>
@@ -11,7 +11,7 @@ export const SocialNetworkProfile =({username, tag, location, avatar, stats}) =>
         className={css.avatar}
       />
       <p className={css.name}>{user.username}</p>
-      <p className={css.tag}>{user.tag}</p>
+        <p className={css.tag}><MdAlternateEmail size={16} viewBox='1 0 20 22'/> {user.tag}</p>
       <p className={css.location}>{user.location}</p>
     </div>
   
