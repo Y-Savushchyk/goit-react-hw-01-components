842492bef48f39656f64344b62419a295390256a
Y-Savushchyk <savushchky@gmail.com>
2023-04-26 22:40:28 +0300

    social profile completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import {SocialNetworkProfile} from 'components/SocialNetworkProfile/SocialNetworkProfile'
+import css from 'components/App.module.css'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <section className={css.section}>
+      <SocialNetworkProfile/>
+    </section>
   );
 };
---
+++ src/components/SocialNetworkProfile/SocialNetworkProfile.jsx
@@ -0,0 +1,37 @@
+import css from './SocialNetworkProfile.module.css';
+import PropTypes from 'prop-types';
+import user from '../../user.json'
+
+export const SocialNetworkProfile =({username, tag, location, avatar, stats}) =>{
+    return (<div className={css.profile}>
+    <div className={css.description}>
+      <img
+        src={user.avatar}
+        alt="User avatar"
+        className={css.avatar}
+      />
+      <p className={css.name}>{user.username}</p>
+      <p className={css.tag}>{user.tag}</p>
+      <p className={css.location}>{user.location}</p>
+    </div>
+  
+    <ul className={css.stats}>
+      <li>
+        <span className={css.label}>Followers</span>
+        <span className={css.quantity}>{user.stats.followers}</span>
+      </li>
+      <li>
+        <span className={css.label}>Views</span>
+        <span className={css.quantity}>{user.stats.views}</span>
+      </li>
+      <li>
+        <span className={css.label}>Likes</span>
+        <span className={css.quantity}>{user.stats.likes}</span>
+      </li>
+    </ul>
+  </div>)
+};
+// ===добавиь пропс для фото
+Event.PropTypes = {
+   username: PropTypes.string,tag: PropTypes.string, location: PropTypes.string, stats: PropTypes.number,
+}
